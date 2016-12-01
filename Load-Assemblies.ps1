35436e2f68c84d65a9412b416ea6d0eda96082c5
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2016-12-01 10:35:05 +0000

    fix assembly loading
    This change attempts to load the portable assembly on powershell
    versions running on coreCLR, and falls back to [Reflection.Assembly]
    
    Fixes #5

--- Load-Assemblies.ps1
+++ Load-Assemblies.ps1
@@ -14,52 +14,22 @@
 #
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$libDir = Join-Path $here "lib"
-
-function ServerLevelKey {
-    <#
-    .SYNOPSIS
-    Returns the path to the registry location where information about the server levels is stored
-    #>
-    PROCESS {
-        return "HKLM:Software\Microsoft\Windows NT\CurrentVersion\Server\ServerLevels"
-    }
-}
-
-function IsNanoServer {
-    <#
-    .SYNOPSIS
-    Return a boolean value of $true if we are running on a Nano server version.
-    #>
-    PROCESS {
-        $serverLevelKey = ServerLevelKey
-        if (!(Test-Path $serverLevelKey)){
-            # We are most likely running on a workstation version
-            return $false
-        }
-        $serverLevels = Get-ItemProperty $serverLevelKey
-        return ($serverLevels.NanoServer -eq 1)
-    }
-}
 
 function Initialize-Assemblies {
-    $isNano = IsNanoServer
-    $assemblyDir = Join-Path $libDir "net35"
-    if($isNano){
-        # Load the portable assembly
-        $assemblyDir = Join-Path $libDir "net45"
+    $libDir = Join-Path $here "lib"
+    $assemblies = @{
+        "portable" = Join-Path $libDir "net45\YamlDotNet.dll";
+        "released" = Join-Path $libDir "net35\YamlDotNet.dll";
     }
-    $assemblyFile = Join-Path $assemblyDir "YamlDotNet.dll"
+
     try {
         [YamlDotNet.Serialization.Serializer] | Out-Null
     } catch [System.Management.Automation.RuntimeException] {
-        if(!(Test-Path $assemblyFile)) {
-            Throw "Could not find YamlDotNet assembly on the system"
+        try {
+            return [Microsoft.PowerShell.CoreCLR.AssemblyExtensions]::LoadFrom($assemblies["portable"])
+        } catch [System.Management.Automation.RuntimeException] {
+            return [Reflection.Assembly]::LoadFrom($assemblies["released"])
         }
-        if($isNano){
-            return [Microsoft.PowerShell.CoreCLR.AssemblyExtensions]::LoadFrom($assemblyFile)
-        }
-        return [Reflection.Assembly]::LoadFrom($assemblyFile)
     }
 }
 
